ed0b0dc02829c11bcc51509c4f238095d9f5cf64
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-30 11:47:40 -0300

    Criando AppDbContext com DbSet da Table Model

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiWeb9.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiWeb9.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<ProdutoModel> Produtos { get; set; }
+    }
+}
