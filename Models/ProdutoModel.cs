fa6089c2f742b756bf5092a2ef7d97068dab994c
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-30 11:48:03 -0300

    Criando Produto Model

---
+++ Models/ProdutoModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiWeb9.Models
+{
+    public class ProdutoModel
+    {
+        public int Id { get; set;  }
+        public string Nome { get; set; } = string.Empty;
+
+        public string Descricao { get; set; } = string.Empty;
+
+        public int QuantidadeEtoque { get; set; }
+
+        public string CodigoDeBarras { get; set; } = string.Empty;
+
+        public string Marca { get; set; } = string.Empty;
+    }
+}
