e2225bbab2fbf9f0771fca48fec8549498f4b9cd
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-31 19:50:08 -0300

    Ajustando a Coluna QuantidadeDeEstoque

---
+++ Migrations/20250531213400_RenameColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiWeb9.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantidadeEtoque",
+                table: "Produtos",
+                newName: "QuantidadeEstoque");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantidadeEstoque",
+                table: "Produtos",
+                newName: "QuantidadeEtoque");
+        }
+    }
+}
