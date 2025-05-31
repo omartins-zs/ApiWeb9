13eb74fa4959fe06e34606119db99a95defe8b2c
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-31 19:50:25 -0300

    Add Coluna de Preco na Table de Produtos

---
+++ Migrations/20250531222553_AddColumnPreco.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiWeb9.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnPreco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Preco",
+                table: "Produtos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Preco",
+                table: "Produtos");
+        }
+    }
+}
